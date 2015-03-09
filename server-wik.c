25b51a81c23fcb8b66beec2012b5b43d344a3474
fgndwikasetiawan <fgn.dwikasetiawan@gmail.com>
2015-03-10 01:01:04 +0700

    Progress server

--- server-wik.c
+++ server-wik.c
@@ -7,26 +7,26 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
-#define BUFSIZ 1080
+#define BUF_SIZ 1080
 #define BACKLOG 10
 #define MAX_NAMELEN 51
 #define len 2048
-
-typedef enum {
-	NAME, MESSAGE
-} state_t;
 
 typedef struct {
 	int sock;
 } thread_args_t;
 
-typedef struct k{
+typedef struct k {
 	char nama[MAX_NAMELEN];
 	int sock;
-	k* next;
+	struct k* next;
 } Klien;
 
 Klien daftarKlien;
+
+short isAlphaNumeric(char c) {
+	return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+}
 
 int start_server(int *sockfd, char* port, int backlog) {
 	struct addrinfo hints, *res;
@@ -45,12 +45,12 @@ int start_server(int *sockfd, char* port, int backlog) {
 	}
 
 	*sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+	status = bind(*sockfd, res->ai_addr, res->ai_addrlen);
 	if (status) {
 		printf("Gagal saat bind!, status: %d\n", status);
 		return(-1);
 	}
-	listen(sockfd, backlog);
+	listen(*sockfd, backlog);
 	return 0;
 }
 
@@ -70,7 +70,7 @@ void populate(Klien *node) {
 	node->next = (Klien*) malloc(sizeof(Klien));
 	node = node->next;
 
-	node->sockfd = 5;
+	node->sock = 5;
 	strcpy(node->nama, "Diana");
 	node->next = NULL;
 }
@@ -84,31 +84,63 @@ int search(const char *nama, Klien *node) {
 	else return -1;
 }
 
+
 void *run(void *t_args) {
 	thread_args_t *args = (thread_args_t*) t_args;
-	char buf[BUFSIZ], msg[BUFSIZ];
+	char buf[BUF_SIZ], msg[BUF_SIZ];
 	char nama[MAX_NAMELEN], namaPenerima[MAX_NAMELEN];
-	int bytes;
+	int bytes, len1;
 
-	//baca nama
-	while ((bytes = recv(args->sock, buf, BUFSIZ-1, 0)) > 0) {
-		buf[bytes] = 0;
+	//kirim daftar user
+	strcpy(buf, "OK\r\n");
+	send(args->sock, buf, strlen(buf), 0);
+	Klien *iterator = &daftarKlien;
+	while (iterator != NULL) {
+		sprintf(buf, "!CONNECT %s\r\n", iterator->nama);
+		send(args->sock, buf, strlen(buf), 0);
+		iterator = iterator->next;
+	}
+
+	//terima nama
+	while ((bytes = recv(args->sock, buf, BUF_SIZ-1, 0)) > 0) {
+		//trimming
+		int i = 0;
+		while (i < bytes && isAlphaNumeric(buf[i]))
+			i++;
+		buf[i] = 0;
+		//selesai trimming
 		strncpy(nama, buf, MAX_NAMELEN-1);
 		int searchResult = search(nama, &daftarKlien);
 		if (searchResult == -1) {
 			strcpy(buf, "OK\r\n");
-			send(args->sock, buf, sizeof(buf), 0);
+			send(args->sock, buf, strlen(buf), 0);
 			break;
 		}
 		else {
 			strcpy(buf, "NOPE\r\n");
-			send(args->sock, buf, sizeof(buf), 0);
+			send(args->sock, buf, strlen(buf), 0);
 		}
 	}
-	//baca command dan sejenisnya
-	while ((bytes = recv(args->sock, buf, BUFSIZ-1, 0)) > 0) {
-		buf[bytes] = 0;
 
+	while ((bytes = recv(args->sock, buf, BUF_SIZ-1, 0)) > 0) {
+		buf[bytes] = 0;
+		char* titikdua = strchr(buf, ':');
+		if (titikdua != NULL) {
+			int indexTD = (int)(titikdua - buf);
+			if (indexTD <= 50) {
+				strncpy(namaPenerima, buf, indexTD);
+			}
+			namaPenerima[indexTD] = 0;
+			int searchResult = search(namaPenerima, &daftarKlien);
+			if (searchResult == -1) {
+				sprintf(buf, "%s tidak online\r\n", namaPenerima);
+				send(args->sock, buf, strlen(buf), 0);
+			}
+			else {
+				sprintf(buf, "%s memiliki nomor socket %d\r\n", namaPenerima, searchResult);
+				send(args->sock, buf, strlen(buf), 0);
+			}
+		}
 	}
 
 	free(args);
@@ -134,9 +166,9 @@ int main (int argc, char* argv[]) {
 	while(1)
 	{
 		new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size);
-		thread_args_t *args = (thread_args_t*) malloc sizeof(thread_args_t);
+		thread_args_t *args = (thread_args_t*) malloc(sizeof(thread_args_t));
 		args->sock = new_fd;
-		run((void*)args);
+		run(args);
 		close(socket);
 	}
 
